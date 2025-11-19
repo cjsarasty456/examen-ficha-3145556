81f59dd0635a0cf58d0f2354aff27b2b1448fb5c
david-erez <erezsantiago25@gmail.com>
2025-11-19 17:32:17 -0500

    Add files via upload

---
+++ David-Santiago-Erez-Rodriguez/EjercicioSis-Siete/src/main/java/com/mycompany/ejerciciosis/siete/EjercicioSisSiete.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciosis.siete;
+
+/**
+ *
+ * @author nox
+ */
+public class EjercicioSeisSiete {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
